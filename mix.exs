45170b0e2dbb852d5faf2977ff5313b5e7114270
Elmar Ismayilov <bamazy@gmail.com>
2023-06-19 11:07:29 +0800

    elixir version changed to that of render

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PaymentApi.MixProject do
     [
       app: :payment_api,
       version: "0.1.0",
-      elixir: "~> 1.14.5",
+      elixir: "~> 1.13.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
