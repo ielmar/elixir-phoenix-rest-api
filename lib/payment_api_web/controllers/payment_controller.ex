91fe7abafff2e57c41cd5ca7ac521211de33e935
Elmar Ismayilov <bamazy@gmail.com>
2023-06-19 16:24:47 +0800

    payload changed

--- lib/payment_api_web/controllers/payment_controller.ex
+++ lib/payment_api_web/controllers/payment_controller.ex
@@ -1,9 +1,9 @@
 defmodule PaymentApiWeb.PaymentController do
   use PaymentApiWeb, :controller
 
-  def create(conn, %{"payment" => payment_params}) do
-    [age, name] = Map.values(payment_params)
+  def create(conn, %{"payload" => payload, "code" => code, "message" => message, "route" => route}) do
+    [version, orderID, sessionId, transactionType] = Map.values(payload)
     conn
-    |> text("POST request successful. Data sent was: #{name}, #{age}")
+    |> text("POST request successful. Data sent was: #{payload}, #{code}, #{message}, #{route}, #{version}, #{orderID}, #{sessionId}, #{transactionType}, #{payload}")
   end
 end
